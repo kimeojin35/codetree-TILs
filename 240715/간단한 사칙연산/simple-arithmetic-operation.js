6a3b0777f252f3779903afbf89c03e0d68f6351b
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-15 10:51:04 +0900

    Title: 간단한 사칙연산, Time: 180ms, Memory: 14MB, Status: Passed - CodeTree

--- 240715/간단한 사칙연산/simple-arithmetic-operation.js
+++ 240715/간단한 사칙연산/simple-arithmetic-operation.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
 const [a, b] = fs.readFileSync(0).toString().split(' ').map(Number)
-console.log(`${a + b}\n${a - b}\n${(a / b).toFixed(0)}\n${a % b}`)
+console.log(`${a + b}\n${a - b}\n${Math.floor(a / b)}\n${a % b}`)
