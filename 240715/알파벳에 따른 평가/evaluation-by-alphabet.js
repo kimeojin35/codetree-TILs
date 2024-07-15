324868a7f8258d072fe2c6226696b6bcce9938d0
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-15 11:54:42 +0900

    Title: 알파벳에 따른 평가, Time: -1ms, Memory: -1MB, Status: Runtime Error - CodeTree

--- 240715/알파벳에 따른 평가/evaluation-by-alphabet.js
+++ 240715/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -1,12 +1,10 @@
 const fs = require('fs')
 const n = fs.readFileSync(0).toString()
 let answer = 'Failure';
-switch(n) {
-    case 'S': answer = 'Superior'; break;
-    case 'A': answer = 'Excellent'; break;
-    case 'B': answer = 'Good'; break;
-    case 'C': answer = 'Usually'; break;
-    case 'D': answer = 'Effort'; break;
-    default: answer = 'Failure'; break;
-}
-console.log(answer)
+if (n == 'S') answer = 'Superior'
+else if (n == 'A') answer = 'Excellent'
+else if (n == 'B')answer = 'Good'; 
+else if (n == 'C') answer = 'Usually'
+else if (n == 'D') answer = 'Effort'
+else answer == 'Failure'
+le.log(answer)
