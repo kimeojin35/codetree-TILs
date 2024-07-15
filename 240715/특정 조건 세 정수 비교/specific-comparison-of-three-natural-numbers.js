e8db70d7979137a6cdafceb36cc9410dc991813b
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-15 12:27:41 +0900

    Title: 특정 조건 세 정수 비교, Time: 165ms, Memory: 14MB, Status: Passed - CodeTree

--- 240715/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
+++ 240715/특정 조건 세 정수 비교/specific-comparison-of-three-natural-numbers.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().split(' ').map(Number)
-console.log(input.filter(i => i >= Math.min.apply(null, input)) == input ? 1 : 0, input[0] == input[1] && input[1] == input[2] ? 1 : 0)
+console.log(input[0] == Math.min.apply(null, input) ? 1 : 0, input[0] == input[1] && input[1] == input[2] ? 1 : 0)
