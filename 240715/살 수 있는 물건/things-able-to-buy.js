5f5d8b5b78776f4ec45b1baf72eae4e351c19e1e
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-15 11:37:16 +0900

    Title: 살 수 있는 물건, Time: 159ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240715/살 수 있는 물건/things-able-to-buy.js
+++ 240715/살 수 있는 물건/things-able-to-buy.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
 const n = Number(fs.readFileSync(0))
-console.log(n >= 3000 ? "book" : (n >= 1000 ? "mask" : ""))
+console.log(n >= 3000 ? "book" : (n >= 1000 && n < 3000 ? "mask" : ""))
