149b7535ea4956676893e89306aaa113a0bda543
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-15 11:26:34 +0900

    Title: 비교에 따른 연산, Time: 225ms, Memory: 15MB, Status: Passed - CodeTree

--- 240715/비교에 따른 연산/operation-based-on-comparison.js
+++ 240715/비교에 따른 연산/operation-based-on-comparison.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
 const [a, b] = fs.readFileSync(0).toString().split(' ').map(Number)
-console.log(a > b ? a * b : Math.floor(b / a)))
+console.log(a > b ? a * b : Math.floor(b / a))
