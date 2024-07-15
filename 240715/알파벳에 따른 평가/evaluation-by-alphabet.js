6d1fc81ab3c16469be64c848452f8be66396b31b
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-15 11:49:02 +0900

    Title: 알파벳에 따른 평가, Time: 171ms, Memory: 14MB, Status: Wrong Answer - CodeTree

---
+++ 240715/알파벳에 따른 평가/evaluation-by-alphabet.js
@@ -0,0 +1,12 @@
+const fs = require('fs')
+const n = fs.readFileSync(0)
+let answer;
+switch(n) {
+    case 'S': answer = 'Superior'; break;
+    case 'A': answer = 'Excellent'; break;
+    case 'B': answer = 'Good'; break;
+    case 'C': answer = 'Usually'; break;
+    case 'D': answer = 'Effort'; break;
+    default: answer = 'Failure'
+}
+console.log(answer)
