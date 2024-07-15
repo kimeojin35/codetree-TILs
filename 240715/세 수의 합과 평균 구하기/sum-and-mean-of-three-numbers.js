6c7e651c2c0b37e0697958c73171397f41424c7f
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-15 11:05:53 +0900

    Title: 세 수의 합과 평균 구하기, Time: 164ms, Memory: 14MB, Status: Passed - CodeTree

--- 240715/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
+++ 240715/세 수의 합과 평균 구하기/sum-and-mean-of-three-numbers.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
 const [a, b, c] = fs.readFileSync(0).toString().split(' ').map(Number)
-console.log(`${a + b + c}\n${((a + b + c) / 3).toFixed(0)}`)
+console.log(`${a + b + c}\n${Math.floor((a + b + c) / 3)}`)
