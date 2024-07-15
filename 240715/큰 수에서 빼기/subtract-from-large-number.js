ceb18eecce9e08a888fddc55d23d5205c9bea1c4
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-15 11:12:54 +0900

    Title: 큰 수에서 빼기, Time: 182ms, Memory: 14MB, Status: Passed - CodeTree

--- 240715/큰 수에서 빼기/subtract-from-large-number.js
+++ 240715/큰 수에서 빼기/subtract-from-large-number.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
 const input = fs.readFileSync(0).toString().split(' ').map(Number)
-console.log(Math.max(input) - Math.min(input))
+console.log(Math.max.apply(null, input) - Math.min.apply(null, input))
