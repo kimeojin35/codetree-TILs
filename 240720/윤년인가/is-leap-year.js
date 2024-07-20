21bde4d46e71101172be6290b5cb2a3127e263db
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-20 21:52:42 +0900

    Title: 윤년인가, Time: 243ms, Memory: 14MB, Status: Passed - CodeTree

--- 240720/윤년인가/is-leap-year.js
+++ 240720/윤년인가/is-leap-year.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
 const input = Number(fs.readFileSync(0))
-console.log(input % 4 == 0 || (input % 100 == 0 && input % 400 != 0))
+console.log(input % 4 == 0 && !(input % 100 == 0 && input % 400 != 0))
