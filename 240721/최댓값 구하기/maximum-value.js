86c349b721055fc45ab729fd1f566cfd6854a176
kimeojin35 <129836778+kimeojin35@users.noreply.github.com>
2024-07-21 23:32:07 +0900

    Title: 최댓값 구하기, Time: 180ms, Memory: 14MB, Status: Wrong Answer - CodeTree

--- 240721/최댓값 구하기/maximum-value.js
+++ 240721/최댓값 구하기/maximum-value.js
@@ -1,3 +1,3 @@
 const fs = require('fs')
-const input = fs.readFileSync(0).split(" ").map(Number)
+const input = fs.readFileSync(0).toString().split(" ").map(Number)
 console.log(Math.max.apply(null, input))
